678427479acc7071f4fb640b546d7fc3a7c83432
Lir0sis <suparboivalik@gmail.com>
2021-10-31 19:55:17 +0200

    lab1-2

--- it92/shenfeld/Lab2.java
+++ it92/shenfeld/Lab2.java
@@ -4,9 +4,13 @@ public class Lab2 {
     public static void main(String[] args) {
         // a x B, a - const
         // type - float
-        // 
+        // Columns, sum of max(odd), sum of min(even)
         final float a = 5;
-        float[][] B = new float[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        float[][] B = new float[][] { 
+            { 1, 2, 3 }, 
+            { 4, 5, 6 }, 
+            { 7, 8, 9 } 
+        };
 
         float[][] C = new float[3][3];
 
@@ -26,26 +30,27 @@ public class Lab2 {
 
         // Even
         for (int i = 0; i < C.length; i += 2) {
-            float min = C[i][0];
+            float min = C[0][i];
             for (int j = 1; j < C[0].length; j++) {
-                var value = C[i][j];
+                var value = C[j][i];
                 if (value < min)
                     min = value;
             }
             sumEven += min;
         }
-
+        System.out.println("MIN in even columns (sum): " + sumEven);
+        
         // Odd
         for (int i = 1; i < C.length; i += 2) {
-            float max = C[i][0];
+            float max = C[0][i];
             for (int j = 1; j < C[0].length; j++) {
-                var value = C[i][j];
+                var value = C[j][i];
                 if (value > max)
                     max = value;
             }
             sumOdd += max;
         }
-        System.out.println("Sum of max element in odd columns: " + sumOdd);
-        System.out.println("Sum of min element in even columns: " + sumEven);
+        System.out.println("MAX in odd columns (sum): " + sumOdd);
+        
     }
 }
